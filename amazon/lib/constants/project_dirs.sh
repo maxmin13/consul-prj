81e13c76f96a0fa173722774ed67f75a148dbc2c
vagrant <vagrant@maxmin.com>
2022-10-27 11:10:57 +0100

    Readme file img

--- amazon/lib/constants/project_dirs.sh
+++ amazon/lib/constants/project_dirs.sh
@@ -5,5 +5,5 @@
 TMP_DIR="${PROJECT_DIR}"/temp
 ACCESS_DIR="${PROJECT_DIR}"/access
 PROVISION_DIR="${PROJECT_DIR}"/programs
-SERVICES_DIR="${PROJECT_DIR}"/provision/docker-services
+SERVICES_DIR="${PROVISION_DIR}"/docker-services
 LOGS_DIR="${PROJECT_DIR}"/logs/$(date +%Y%m%d)
